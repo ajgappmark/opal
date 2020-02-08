a40befd3d1c230f66d436173f5a7fe7f2b8e362c
Elia Schito <elia@schito.me>
2020-02-08 16:30:04 +0100

    Move the type_error helper to the runtime

--- opal/corelib/helpers.rb
+++ opal/corelib/helpers.rb
@@ -1,14 +1,8 @@
+# helpers: type_error
+
 module Opal
   def self.bridge(constructor, klass)
     `Opal.bridge(constructor, klass)`
-  end
-
-  def self.type_error(object, type, method = nil, coerced = nil)
-    if method && coerced
-      TypeError.new "can't convert #{object.class} into #{type} (#{object.class}##{method} gives #{coerced.class})"
-    else
-      TypeError.new "no implicit conversion of #{object.class} into #{type}"
-    end
   end
 
   def self.coerce_to(object, type, method, *args)
@@ -25,7 +19,7 @@ module Opal
     coerced = coerce_to(object, type, method, *args)
 
     unless type === coerced
-      raise type_error(object, type, method, coerced)
+      raise `$type_error(object, type, method, coerced)`
     end
 
     coerced
@@ -39,7 +33,7 @@ module Opal
     return if coerced.nil?
 
     unless type === coerced
-      raise type_error(object, type, method, coerced)
+      raise `$type_error(object, type, method, coerced)`
     end
 
     coerced
